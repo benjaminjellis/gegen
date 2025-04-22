1fdaf80aec428118ad869b72bd44616b458e950d
benjamin <32769527+benjaminjellis@users.noreply.github.com>
2025-04-23 00:39:08 +0100

    fix: api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use chrono::{DateTime, Utc, serde::ts_seconds};
 use const_format::concatcp;
 use reqwest::{Client, header::HeaderMap};
@@ -38,47 +40,136 @@ fn create_header_maps() -> HeaderMap {
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct LiveScoresResponse {
-    matches: Vec<Match>,
+pub struct LiveScoresResponse {
+    pub matches: Vec<Match>,
 }
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct Match {
-    id: String,
-    status: Status,
-    comp: Competition,
+pub struct Match {
+    pub id: String,
+    pub status: Status,
+    pub comp: Competition,
     #[serde(with = "ts_seconds")]
-    date: DateTime<Utc>,
-    home: Team,
-    away: Team,
+    pub date: DateTime<Utc>,
+    pub home: Team,
+    pub away: Team,
+    pub score: HashMap<ScoreKey, Score>,
+    pub events: Vec<Event>,
+
+    #[serde(with = "ts_seconds")]
+    pub updated: DateTime<Utc>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase", tag = "entity_type")]
+pub enum Event {
+    Sub(SubEvent),
+    Goal(GoalEvent),
+    Card(CardEvent),
+    Var,
 }
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct Competition {
-    id: String,
-    name: String,
-    country: Country,
+pub struct CardEvent {
+    pub period_id: u8,
+    pub min: u16,
+    pub time_str: String,
+    pub team_id: String,
+    #[serde(rename = "type")]
+    pub card_type: CardType,
+}
+
+#[derive(Debug, Deserialize)]
+pub enum CardType {
+    #[serde(rename = "YC")]
+    YellowCard,
+    #[serde(rename = "Y2C")]
+    SecondYellowCard,
+    #[serde(rename = "RC")]
+    RedCard,
 }
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct Country {
-    id: String,
-    full_name: String,
+pub struct GoalEvent {
+    pub period_id: u8,
+    pub min: u16,
+    pub time_str: String,
+    pub team_id: String,
+    pub player_id: String,
+    pub player_name: String,
+    #[serde(rename = "type")]
+    pub goal_type: GoalType,
+    pub score: [u8; 2],
+}
+
+#[derive(Debug, Deserialize)]
+pub enum GoalType {
+    #[serde(rename = "G")]
+    Goal,
+    #[serde(rename = "PG")]
+    Penalty,
+    #[serde(rename = "OG")]
+    OwnGoal,
 }
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct Team {
-    id: String,
-    name: String,
+pub struct SubEvent {
+    pub period_id: u8,
+    pub min: u16,
+    pub time_str: String,
+    pub team_id: String,
+    pub player_id: String,
+    pub player_name: String,
+    pub player2_id: String,
+    pub player2_name: String,
+}
+
+#[derive(Hash, Eq, PartialEq, PartialOrd, Ord, Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub enum ScoreKey {
+    Ft,
+    Ht,
+    Total,
+    Aggregate,
+    TotalUnconfirmed,
 }
 
 #[derive(Debug, Deserialize)]
-#[serde(rename_all = "lowercase")]
-enum Status {
+#[serde(rename_all = "camelCase")]
+pub struct Score {
+    pub home: u8,
+    pub away: u8,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Competition {
+    pub id: String,
+    pub name: String,
+    pub country: Country,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Country {
+    pub id: String,
+    pub full_name: String,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Team {
+    pub id: String,
+    pub name: String,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub enum Status {
     Played,
     Fixture,
     Playing,
